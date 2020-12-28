675396b6300e8f2b75cc6f86ced27e33ebcfdc64
Swayam <74960567+practice404@users.noreply.github.com>
2020-12-28 22:44:00 +0530

    newsletter sign up using mailchimp
    mailchimp API + express + https + body-parser + node
    
    (Do not make typos and read Documentation carefully)

---
+++ news_letter/app.js
@@ -0,0 +1,97 @@
+// jshint esversion:6
+
+const express = require("express");
+const bodyparser = require("body-parser");
+const https = require("https");
+const request = require("request");
+
+const app = express();
+
+app.use(express.static("public"));
+app.use(bodyparser.urlencoded({extended:true}));
+
+app.get("/", function(req,res) {
+  res.sendFile(__dirname+"/signup.html");
+});
+
+app.post("/", function(req,res) {
+  let fname = req.body.fname;
+  let lname = req.body.lname;
+  let email = req.body.email;
+
+  const data = {
+
+  members: [
+
+    {
+
+      email_address: email,
+
+      status: "subscribed",
+
+      merge_fields: {
+
+        FNAME: fname,
+
+        LNAME: lname,
+
+      }
+
+
+
+    }
+
+  ]
+
+};
+
+
+const jsondata = JSON.stringify(data);
+
+const url = "https://us7.api.mailchimp.com/3.0/lists/<list_id>";
+
+const options = {
+
+   method: "POST",
+
+   auth: "key:<API_KEY>",
+
+ };
+
+
+ const request = https.request(url,options,function(response){
+
+   if (response.statusCode == 200)
+   {
+     res.sendFile(__dirname+"/success.html");
+   }
+   else {
+     res.sendFile(__dirname+"/failure.html");
+   }
+
+  response.on("data", function(data){
+
+    console.log(JSON.parse(data));
+
+  });
+
+});
+
+
+
+request.write(jsondata);
+
+request.end();
+
+
+
+});
+
+
+app.post("/failure", function(req,res){
+  res.redirect("/");
+});
+
+app.listen(process.env.PORT || 5000, function(){
+  console.log("server is running on port 5000");
+});
